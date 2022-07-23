d46badca0dab76c67657bf6e4eac296b2296e430
omer <omer_gocmen68@outlook.com>
2022-07-23 18:20:36 +0300

    EntityFramework için soyutlama ve Core katmanının oluşturulması yapıldı.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,8 +9,23 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            //ProductTest();
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var item in productManager.GeyByUnitPrice(40,100))
+            foreach (var item in productManager.GeyByUnitPrice(40, 100))
             {
                 Console.WriteLine(item.ProductName);
             }
