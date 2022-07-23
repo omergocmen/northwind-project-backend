1b4a422b4e85c3ca5c49e407904726e4126bf8be
omer <omer_gocmen68@outlook.com>
2022-07-23 14:06:54 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GeyByUnitPrice(40,100))
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+    }
+}
